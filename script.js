d3480bc35242fb8ce051cf239c6ef4a30b33d239
Alper Colak <colakalpeer93@gmail.com>
2024-08-25 23:27:59 +0300

    feat: document selected

--- script.js
+++ script.js
@@ -18,3 +18,5 @@ const $header = document.querySelector("[data-header]");
 windows.addEventListener("scroll", (e) => {
   $header.classList[windows.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+
